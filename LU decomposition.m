9ec4ae2fc1191e62027fe764e3a77507035bed3e
wy7318 <46336956+wy7318@users.noreply.github.com>
2019-01-29 18:45:47 -0700

    Create LU decomposition.m

---
+++ LU decomposition.m
@@ -0,0 +1,11 @@
+function [L,U] = mylu(A)
+n = size(A,1);
+for k=1:n
+    if A(k,k)==0
+        warning('LU factorization fails'); L=[]; U=[]; return
+    end
+    i=k+1:n;
+    A(i,k)=A(i,k)/A(k,k);
+    A(i,i)=A(i,i)-A(i,k)*A(k,i);
+end
+L=tril(A,-1)+eye(n); U=triu(A);
